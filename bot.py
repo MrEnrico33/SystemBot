682e366b75cc386f0da9b81bd8c0569b0aafeb8c
MrEnrico33 <44499384+MrEnrico33@users.noreply.github.com>
2019-05-10 14:19:23 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ client = discord.Client()
 def on_ready():
 
     print ("Bot ist bereit")
-    yield from client.change_presence(game=Game(name="#with the Kings"))
+    yield from client.change_presence(game=Game(name="#with the Kings", type = 3))
     
 
 
